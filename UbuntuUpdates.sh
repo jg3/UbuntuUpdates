e8cffe85ef0e2b2f200ba3c9d268b3a6ad59c0e1
jg3 <jg3@topsecret.net>
2018-02-19 15:58:23 -0500

    Import from previous script
    Found most of this somewhere on GitHub  ketp the colors and echo statements, edited all the actual commands.

---
+++ UbuntuUpdates.sh
@@ -0,0 +1,48 @@
+#!/bin/bash
+
+TEXT_RESET='\e[0m'
+TEXT_YELLOW='\e[0;33m'
+TEXT_RED_B='\e[1;31m'
+
+# this part is really just here for the sudo
+# and we assume that
+#  . sudo on your system stays cached (default 5min)
+#  .
+echo -e $TEXT_YELLOW
+sudo echo "Begin Ubuntu update script ..."
+echo -e $TEXT_RESET
+
+
+sudo apt-get -y update
+echo -e $TEXT_YELLOW
+echo 'APT update finished...'
+echo -e $TEXT_RESET
+
+sudo apt-get -y dist-upgrade
+echo -e $TEXT_YELLOW
+echo 'APT distributive upgrade finished...'
+echo -e $TEXT_RESET
+
+sudo apt-get -y upgrade
+echo -e $TEXT_YELLOW
+echo 'APT upgrade finished...'
+echo -e $TEXT_RESET
+
+sudo apt-get -y autoremove
+echo -e $TEXT_YELLOW
+echo 'APT auto remove finished...'
+echo -e $TEXT_RESET
+
+if [ -f /var/run/reboot-required ]; then
+    echo -e $TEXT_RED_B
+    echo 'Reboot required!'
+    echo -e $TEXT_RESET
+fi
+
+echo "Do you wish to reboot?"
+select yn in "Yes" "No"; do
+    case $yn in
+        Yes ) sudo reboot;;
+        No ) exit;;
+    esac
+done
