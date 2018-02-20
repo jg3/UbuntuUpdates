fe712826bda673a76efb5c980d7b48b20a7fcc58
jg3 <jg3@topsecret.net>
2018-02-20 10:32:16 -0500

    added bash test in at the beginning.  tieded some of the post-run checks.

--- UbuntuUpdates.sh
+++ UbuntuUpdates.sh
@@ -1,5 +1,11 @@
 #!/bin/bash
 
+if [ !$BASH ] ; then
+  echo "Calling this script under sh will not work as well."
+  echo "Either call as \"bash ./$1\" or do \"chmod u+x $1\" "
+  echo "and then run it normally."
+fi
+
 TEXT_RESET='\e[0m'
 TEXT_YELLOW='\e[0;33m'
 TEXT_RED_B='\e[1;31m'
@@ -33,19 +39,22 @@ echo -e $TEXT_YELLOW
 echo 'APT auto remove finished...'
 echo -e $TEXT_RESET
 
+# run the check shown in the MOTD to show the new status.
+echo -e $TEXT_RED_B
+/usr/lib/update-notifier/apt_check.py --human-readable
+echo -e $TEXT_RESET
+
 if [ -f /var/run/reboot-required ]; then
     echo -e $TEXT_RED_B
     echo 'Reboot required!'
     echo -e $TEXT_RESET
 fi
 
-# run the check shown in the MOTD to show the new status.
-/usr/lib/update-notifier/apt_check.py --human-readable
-
 echo "Do you wish to reboot?"
 select yn in "Yes" "No"; do
     case $yn in
         Yes ) sudo reboot;;
         No ) exit;;
+        * ) echo "pick a number 1, 2"
     esac
 done
