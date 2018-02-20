ab57530ab3e9143e0b9b39898b78b9cc682fb4b8
jg3 <jg3@topsecret.net>
2018-02-20 10:50:14 -0500

    1 != 0   ... remember that.

--- UbuntuUpdates.sh
+++ UbuntuUpdates.sh
@@ -2,7 +2,7 @@
 
 if [ !$BASH ] ; then
   echo "Calling this script under sh will not work as well."
-  echo "Either call as \"bash ./$1\" or do \"chmod u+x $1\" "
+  echo "Either call as \"bash ./$0\" or do \"chmod u+x $0\" "
   echo "and then run it normally."
   exit
 fi
